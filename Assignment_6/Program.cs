e2c7e5bc294d8214d642fddb24c860ef9752b6c9
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-12 05:06:51 +0300

    Ceate a Shipment struct

--- Assignment_6/Program.cs
+++ Assignment_6/Program.cs
@@ -2,8 +2,8 @@
 {
     internal class Program
     {
-       // public struct DeliveryAddress
-        // {
+        //public struct DeliveryAddress
+        //{
         //    public string City;
         //    public string Street;
         //    public int BuildingNumber;
@@ -20,6 +20,83 @@
         //        return $"{BuildingNumber} {Street}, {City}";
         //    }
         //}
+
+        #region Ceate a Shipment struct
+        //public struct Shipment
+        //{
+        //    private string trackingCode;
+        //    private string description;
+        //    private double weight;
+        //    private decimal deliveryFee;
+
+        //    public Shipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
+        //    {
+
+        //        this.trackingCode = string.IsNullOrWhiteSpace(trackingCode) ? "UNVALID" : trackingCode;
+        //        this.description = string.IsNullOrWhiteSpace(description) ? "NVALID" : description;
+        //        this.weight = weight > 0 ? weight : 1.0;
+        //        this.deliveryFee = deliveryFee > 0 ? deliveryFee : 10.0m;
+        //        Destination = destination;
+        //    }
+
+        //    public string TrackingCode
+        //    {
+        //        get { return trackingCode; }
+        //        private set
+        //        {
+        //            if (!string.IsNullOrWhiteSpace(value))
+        //            {
+        //                trackingCode = value;
+        //            }
+        //        }
+        //    }
+
+        //    public string Description
+        //    {
+        //        get { return description; }
+        //        set
+        //        {
+        //            if (!string.IsNullOrWhiteSpace(value))
+        //            {
+        //                description = value;
+        //            }
+        //        }
+        //    }
+
+        //    public double Weight
+        //    {
+        //        get { return weight; }
+        //        set
+        //        {
+        //            if (value > 0)
+        //            {
+        //                weight = value;
+        //            }
+        //        }
+        //    }
+
+        //    public decimal DeliveryFee
+        //    {
+        //        get { return deliveryFee; }
+        //        private set
+        //        {
+        //            if (value > 0)
+        //            {
+        //                deliveryFee = value;
+        //            }
+        //        }
+        //    }
+
+        //    public DeliveryAddress Destination { get; set; }
+        //    public decimal EstimatedCost
+        //    {
+        //        get
+        //        {
+        //            return deliveryFee + ((decimal)weight * 5m);
+        //        }
+        //    }
+        //}
+        #endregion
         static void Main(string[] args)
         {
             #region Question 1
@@ -55,6 +132,10 @@
             //Console.WriteLine($"Copied Address: {copiedAddress.GetFullAddress()}");
             #endregion
 
+
+   
+            
+         
         }
     }
 }
