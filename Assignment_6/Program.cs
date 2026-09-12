27992673740d6015417a8f60620eb238440839e1
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-12 04:20:32 +0300

    Question 2

--- Assignment_6/Program.cs
+++ Assignment_6/Program.cs
@@ -12,6 +12,20 @@
             //Modifying the object through either them affects both the original and the copy.
             #endregion
 
+
+            #region Question 2
+            // a)
+            // The 3 fields are public, breaking data hiding by allowing external code to modify them directly.
+            // No validation or constraints can be enforced on the values assigned to these fields.
+            // Internal values cannot be protected as read-only or computed properties so its better to use calss instead of struct for data hiding and encapsulation.
+
+            //b)
+            // Data hiding, protection & encapsulation are important
+            // input validation through properties like set & get methods
+            // fliexibility and read only properties for internal values
+            #endregion
+
+
         }
     }
 }
